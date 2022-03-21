0ef8c4e22181dce4beeb26243e45c29dd273deda
fifiikompaniq <yosif.g.saltiel.2020@elsys-bg.org>
2022-03-21 17:55:41 +0200

    Update recognise.py

--- main/recognise.py
+++ main/recognise.py
@@ -1,9 +1,17 @@
-import pstats
+import speech_recognition as sr
+from moviepy.editor import *
 
+video_clip = VideoFileClip("E:\Subtitle-Filmmaker\main\\test.mp4")
+audio_clip = video_clip.audio
 
-class Recogniser: 
-    pass
+audio_clip.write_audiofile("audio_file.mp3", 44100, 2, 2000)
+recognizer = sr.Recognizer()
 
-class Subtitler: 
-    pass 
-
+with sr.AudioFile("audio_file.mp3") as source: 
+    audio = recognizer.record(source)
+    
+try: 
+    with open("transcribe.txt", "w+") as transcript: 
+        transcript.write("{}".format(recognizer.recognize_sphinx(audio)))
+except sr.UnknownValueError: 
+    print("Could not understand audio.")
