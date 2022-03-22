9e97ec962aa9ca8ad6469a484441c7819e9ad6e0
yosko123 <yosif.g.saltiel.2020@elsys-bg.org>
2022-03-22 22:05:14 +0200

    updated

--- main/recognise.py
+++ main/recognise.py
@@ -1,17 +1,24 @@
 import speech_recognition as sr
 from moviepy.editor import *
+from os import path
+from pydub import AudioSegment
 
 video_clip = VideoFileClip("E:\Subtitle-Filmmaker\main\\test.mp4")
 audio_clip = video_clip.audio
+dst = "test.wav"
 
 audio_clip.write_audiofile("audio_file.mp3", 44100, 2, 2000)
 recognizer = sr.Recognizer()
+audio_src = "audio_file.mp3"
+sound = AudioSegment.from_mp3(audio_src)
+sound.export(dst, format="wav")
 
-with sr.AudioFile("audio_file.mp3") as source: 
+
+with sr.AudioFile("test.wav") as source: 
     audio = recognizer.record(source)
-    
+
 try: 
     with open("transcribe.txt", "w+") as transcript: 
-        transcript.write("{}".format(recognizer.recognize_sphinx(audio)))
+        transcript.write("{}".format(recognizer.recognize_google_cloud(audio)))
 except sr.UnknownValueError: 
     print("Could not understand audio.")
