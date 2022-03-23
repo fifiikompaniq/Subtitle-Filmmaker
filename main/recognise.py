0382cffefaaa667959502eca5cb2aad1e63fc8b5
yosko123 <yosif.g.saltiel.2020@elsys-bg.org>
2022-03-24 00:09:19 +0200

    PCM WAV

--- main/recognise.py
+++ main/recognise.py
@@ -5,20 +5,19 @@ from pydub import AudioSegment
 
 video_clip = VideoFileClip("E:\Subtitle-Filmmaker\main\\test.mp4")
 audio_clip = video_clip.audio
-dst = "test.wav"
 
-audio_clip.write_audiofile("audio_file.mp3", 44100, 2, 2000)
+
+audio_clip.write_audiofile("audio_file.wav", 44100, 2, 2000)
 recognizer = sr.Recognizer()
-audio_src = "audio_file.mp3"
-sound = AudioSegment.from_mp3(audio_src)
-sound.export(dst, format="wav")
+ 
 
 
-with sr.AudioFile("test.wav") as source: 
+with sr.AudioFile("audio_file.wav") as source: 
+    recognizer.adjust_for_ambient_noise(source, duration=1)
     audio = recognizer.record(source)
 
 try: 
     with open("transcribe.txt", "w+") as transcript: 
-        transcript.write("{}".format(recognizer.recognize_google_cloud(audio)))
+        transcript.write("{}".format(recognizer.recognize_sphinx(audio)))
 except sr.UnknownValueError: 
     print("Could not understand audio.")
