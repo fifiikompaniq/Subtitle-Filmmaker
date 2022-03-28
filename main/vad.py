ae883794f319a3648a2f98062ee439a55f62f3e1
fifiikompaniq <yosif.g.saltiel.2020@elsys-bg.org>
2022-03-28 16:18:58 +0300

    Update vad.py

--- main/vad.py
+++ main/vad.py
@@ -1,11 +1,6 @@
 """
 
-    Takes an audio file -> returns a file with the timestamps of the speech detection
-    Using a moving frame-checker, we loop through the audio file (PCM WAV FORMAT)
-    and we check each 30ms whether the VAD returns True or False. 
-    If True, if there's no timestamp on the vector(index 1, index 2)
-    we collect it on index 1. 
-    If False, we collect the timestamp on index 2 and we export the tuple.  
+    Using silero-vad we can easily collect the timestamps.
 
 """
 import torch
